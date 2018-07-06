792719c1a70df037520d10d65fab7a583b9da31c
garyuu <shuhanrpg@gmail.com>
2018-07-06 16:31:47 +0800

    Initialization

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node poi.js
---
+++ poi.js
@@ -0,0 +1,14 @@
+const discord = require('discord.js');
+const client = new discord.Client();
+
+client.on("ready", () => {
+    console.log("I am ready Poi!");
+});
+
+client.on("message", (message) => {
+    if (message.content.startsWith("Poi, ")) {
+        message.channel.send("Poi!!");
+    }
+});
+
+client.login(process.env.BOT_TOKEN)
