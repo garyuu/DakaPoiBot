ccdab162bcb6dc155bf6ba4d7125998d80749f68
garyuu <shuhanrpg@gmail.com>
2018-07-10 15:14:47 +0800

    Add switch-case command processor. Add language mapping config file.

--- poi.js
+++ poi.js
@@ -1,15 +1,25 @@
 require('dotenv').config();
 const discord = require('discord.js');
 const client = new discord.Client();
+const prefix = process.env.BOT_PREFIX;
+const lang = require('./' + process.env.BOT_LANG + '.json');
 
 client.on("ready", () => {
-    console.log("I am ready Poi!");
+    console.log(lang.system.ready);
 });
 
 client.on("message", (message) => {
-    if (message.content.startsWith("Poi, ")) {
-        message.channel.send("Poi!!");
+    if (message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).split(' ');
+        const command = args.shift().toLowerCase();
+        switch (command) {
+            case 'hello':
+                message.channel.send(lang.response.hello);
+                break;
+            default:
+                message.channel.send(lang.response.default);
+        }
     }
 });
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
