8c46fc505df8a7c27300849566d06e1a667810d2
garyuu <shuhanrpg@gmail.com>
2018-12-14 11:52:54 +0800

    fix

--- dirtyWordGenerator.py
+++ dirtyWordGenerator.py
@@ -11,4 +11,4 @@ for i in range(len(inputStr)):
     for j in range(random.randint(1,2)):
       symbol = random.randint(0x0300, 0x0370)
       outputStr += ''.join(chr(symbol))
-print(outputStr)
+print(outputStr.encode('utf-8', 'ignore').decode('utf-8'))
