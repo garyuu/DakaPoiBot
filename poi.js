0fa57132a37164c51778ef1442b28037d2ce1b95
garyuu <shuhanrpg@gmail.com>
2018-12-14 16:16:20 +0800

    Dirty ignore <>

--- poi.js
+++ poi.js
@@ -220,8 +220,14 @@ client.on("message", (message) => {
             case 'dirty':
                 const msg = message.content.slice(prefix.length+command.length+1).trim();
                 let result = "";
+                let skip = false;
                 for (let i = 0; i < msg.length; i++) {
-                    result += msg[i] + (msg.charCodeAt(i) < 0x0080 ? '' : String.fromCharCode(0x0489));
+                    if (skip || msg.charAt(i) == '<') {
+                        skip = msg.charAt(i) != '>'
+                        result += msg.charAt(i);
+                        continue;
+                    }
+                    result += msg.charAt(i) + (msg.charCodeAt(i) < 0x0080 ? '' : String.fromCharCode(0x0489));
                     const rnd = Math.floor(Math.random() * 10) + 1;
                     for (let j = 0; j < rnd; j++) {
                         const char = Math.floor(Math.random() * (0x0370-0x0300)) + 0x0300;
