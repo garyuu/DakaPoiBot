ba04cdc04111fffe0e394d5cb1a77608eb4d14aa
garyuu <shuhanrpg@gmail.com>
2018-12-14 12:02:26 +0800

    fix3

--- dirtyWordGenerator.py
+++ dirtyWordGenerator.py
@@ -6,10 +6,10 @@ try:
     name, inputStr = argv
 except:
     inputStr = input("Input text: ")
-outputStr = ""
+outputStr = u'';
 for i in range(len(inputStr)):
     outputStr += inputStr[i] + ''.join(chr(0x0489))
     for j in range(random.randint(1,2)):
       symbol = random.randint(0x0300, 0x0370)
       outputStr += ''.join(chr(symbol))
-print(outputStr.encode('utf-8', 'ignore').decode('utf-8'))
+print(outputStr)
