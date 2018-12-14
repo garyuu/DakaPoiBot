feec1cf58cea9be89fef1ad97a3c47b8d528b810
garyuu <shuhanrpg@gmail.com>
2018-12-14 13:46:44 +0800

    fix6

--- dirtyWordGenerator.py
+++ dirtyWordGenerator.py
@@ -10,8 +10,8 @@ except:
     inputStr = input("Input text: ")
 outputStr = ''
 for i in range(len(inputStr)):
-    outputStr += inputStr[i] + ''.join(chr(0x0489))
-    for j in range(random.randint(1,2)):
+    outputStr += inputStr[i]# + ''.join(chr(0x0489))
+    for j in range(random.randint(1,10)):
       symbol = random.randint(0x0300, 0x0370)
       outputStr += ''.join(chr(symbol))
 print(outputStr)
